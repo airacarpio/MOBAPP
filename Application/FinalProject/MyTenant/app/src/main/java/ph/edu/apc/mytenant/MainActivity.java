3477deb48ec1a1a5a883b1f45816f6d04aff85c3
airacarpio <airajoyce04@gmail.com>
2016-12-02 20:49:08 +0800

    Final Project My Tenant

---
+++ Application/FinalProject/MyTenant/app/src/main/java/ph/edu/apc/mytenant/MainActivity.java
@@ -0,0 +1,11 @@
+package ph.edu.apc.mytenant;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Aira Joyce on 11/30/2016.
+ */
+public class MainActivity extends AppCompatActivity {
+
+}
