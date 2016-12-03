1f360d5d1deea97fba781b06cf4a1543821ffc71
airacarpio <airajoyce04@gmail.com>
2016-12-03 10:02:10 +0800

    commit app folder

---
+++ Application/FinalProject/MyTenant/app/src/main/java/ph/edu/apc/mytenant/Header_Activity.java
@@ -0,0 +1,29 @@
+package ph.edu.apc.mytenant;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+/**
+ * Created by Aira Joyce on 12/3/2016.
+ */
+
+public class Header_Activity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private TextView tvUserEmail;
+    @Override
+    protected void onCreate(Bundle savedInstancesState) {
+        super.onCreate(savedInstancesState);
+        setContentView(R.layout.nav_header_content_);
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        tvUserEmail=(TextView) findViewById(R.id.tvUserEmail);
+        tvUserEmail.setText("Welcome "+ user.getEmail() );
+
+    }
+}
